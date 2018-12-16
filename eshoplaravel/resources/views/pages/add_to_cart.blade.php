af787e8112ee515b6d0d3505f078a1a97d5a0a0e
rdiaz <rdiaz@ncq.co.cr>
2018-12-16 16:37:40 -0600

    ..

--- eshoplaravel/resources/views/pages/add_to_cart.blade.php
+++ eshoplaravel/resources/views/pages/add_to_cart.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
 <section id="cart_items">
-      <div class="container">
+      <div class="container col-sm-12">
             <div class="breadcrumbs">
                   <ol class="breadcrumb">
                         <li><a href="#">Home</a></li>
@@ -11,6 +11,8 @@
             <div class="table-responsive cart_info">
                   <?php
                         $contents = Cart::getContent();
+                        $cartTotal = Cart::getTotal();
+                        $cartSubTotal = Cart::getSubTotal();
                         //echo "<pre>";
                         //print_r($contents);
                         //echo "</pre>";
@@ -28,6 +30,11 @@
                         </thead>
                         <tbody>
                               @foreach($contents as $v_contents)
+                              <?php
+                                    $cantidad = $v_contents->quantity;
+                                    $precio = $v_contents->price;
+                                    $totallinea =  $cantidad * $precio;
+                              ?>
                               <tr>
                                     <td class="cart_product">
                                           <a href=""><img src="{{URL::to($v_contents->attributes->image)}}" 
@@ -35,10 +42,10 @@
                                     </td>
                                     <td class="cart_description">
                                           <h4><a href="">{{$v_contents->name}}</a></h4>
-                                          <p>Web ID: {{$v_contents->id}}</p>
+                                          <!--<p>Web ID: {{$v_contents->id}}</p>-->
                                     </td>
-                                    <td class="cart_price">
-                                          <p>{{$v_contents->price}}</p>
+                                    <td class="cart_description">
+                                          <h4>{{$v_contents->price}}</h4>
                                     </td>
                                     <td class="cart_quantity">
                                           <div class="cart_quantity_button">
@@ -48,7 +55,7 @@
                                           </div>
                                     </td>
                                     <td class="cart_total">
-                                          <p class="cart_total_price">$59</p>
+                                          <h4 class="cart_total_price">{{$totallinea}}</h4>
                                     </td>
                                     <td class="cart_delete">
                                           <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
@@ -62,73 +69,20 @@
 </section> <!--/#cart_items-->
 
 <section id="do_action">
-      <div class="container">
+      <div class="container col-sm-12">
             <div class="heading">
                   <h3>What would you like to do next?</h3>
                   <p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
             </div>
             <div class="row">
-                  <div class="col-sm-6">
-                        <div class="chose_area">
-                              <ul class="user_option">
-                                    <li>
-                                          <input type="checkbox">
-                                          <label>Use Coupon Code</label>
-                                    </li>
-                                    <li>
-                                          <input type="checkbox">
-                                          <label>Use Gift Voucher</label>
-                                    </li>
-                                    <li>
-                                          <input type="checkbox">
-                                          <label>Estimate Shipping & Taxes</label>
-                                    </li>
-                              </ul>
-                              <ul class="user_info">
-                                    <li class="single_field">
-                                          <label>Country:</label>
-                                          <select>
-                                                <option>United States</option>
-                                                <option>Bangladesh</option>
-                                                <option>UK</option>
-                                                <option>India</option>
-                                                <option>Pakistan</option>
-                                                <option>Ucrane</option>
-                                                <option>Canada</option>
-                                                <option>Dubai</option>
-                                          </select>
-                                          
-                                    </li>
-                                    <li class="single_field">
-                                          <label>Region / State:</label>
-                                          <select>
-                                                <option>Select</option>
-                                                <option>Dhaka</option>
-                                                <option>London</option>
-                                                <option>Dillih</option>
-                                                <option>Lahore</option>
-                                                <option>Alaska</option>
-                                                <option>Canada</option>
-                                                <option>Dubai</option>
-                                          </select>
-                                    
-                                    </li>
-                                    <li class="single_field zip-field">
-                                          <label>Zip Code:</label>
-                                          <input type="text">
-                                    </li>
-                              </ul>
-                              <a class="btn btn-default update" href="">Get Quotes</a>
-                              <a class="btn btn-default check_out" href="">Continue</a>
-                        </div>
-                  </div>
-                  <div class="col-sm-6">
+                  
+                  <div class="col-sm-8">
                         <div class="total_area">
                               <ul>
-                                    <li>Cart Sub Total <span>$59</span></li>
-                                    <li>Eco Tax <span>$2</span></li>
+                                    <li>Cart Sub Total <span>{{Cart::getSubTotal()}}</span></li>
+                                    <li>Eco Tax <span>0</span></li>
                                     <li>Shipping Cost <span>Free</span></li>
-                                    <li>Total <span>$61</span></li>
+                                    <li>Total <span>{{Cart::getTotal()}}</span></li>
                               </ul>
                                     <a class="btn btn-default update" href="">Update</a>
                                     <a class="btn btn-default check_out" href="">Check Out</a>
